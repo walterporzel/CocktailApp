4f1b565ee934f82532b07ffdff15a2281da89601
Walter_Porzel <walterp@vt.edu>
2019-10-03 10:22:41 -0400

    update form working

--- project3-front/src/App.js
+++ project3-front/src/App.js
@@ -6,6 +6,7 @@ import CocktailDetail from "../src/CocktailDetail"
 import IngredientDetail from "./IngredientDetail";
 import { Route, Link } from "react-router-dom";
 import Create from "./create";
+import Update from "./update";
 
 class App extends Component {
   render() {
@@ -20,16 +21,22 @@ class App extends Component {
             <Link to="/ingredients">
               <h3>Ingredients</h3>
             </Link>               
-            <Link to="/create">Create</Link>
-
+            <Link to="/create">
+              <h3>Create Cocktail</h3>
+            </Link>
+            <Link to="/update">
+              <h3>Update Cocktail</h3>
+            </Link>
           </nav>
         </div>
           <main>
-          <Route path="/cocktails" exact component = {Cocktails} />
+          <Route path="/cocktails" 
+          exact component = {Cocktails} />
           <Route path="/ingredients" exact component = {Ingredients} />
           <Route path="/cocktails/:cocktail" exact component = {CocktailDetail} />
           <Route path="/ingredients/:ingredient" exact component ={IngredientDetail}/>
           <Route path="/create" exact component = {Create} />
+          <Route path="/update" exact component = {Update} />
         </main>
       </div>
     );
