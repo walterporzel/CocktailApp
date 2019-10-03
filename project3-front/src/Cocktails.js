6e0cf2214eea9570406787265a92c55957a367e7
Domo <delaney_dominique@icloud.com>
2019-10-03 11:12:41 -0400

    searchfield workingd

--- project3-front/src/Cocktails.js
+++ project3-front/src/Cocktails.js
@@ -23,25 +23,45 @@ class Cocktails extends Component {
         .then(results => {
             return results.json();
         }).then(data => {
-            let cocktails = data.map((drink) => {
-                return(
-                    <Link to={"/cocktails/" + drink.strDrink}><Cocktail drink={drink} /></Link>
-                )
-            })
-            this.setState({cocktails: cocktails});
+            
+            this.setState({cocktails: data});
         })
     }
 
-    onSearchChange(event) {
+    onSearchChange = (event) => {
         this.setState({searchfield:event.target.value})
-        const filteredCocktails = this.state.cocktails.filter(cocktail => {
-            return cocktail.strDrink.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase)
+        // const filteredCocktails = this.state.cocktails.filter(cocktail => {
+        //     return cocktail.strDrink.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase)
                 
-        })
-        console.log(filteredCocktails)
+        // })
+        // console.log(filteredCocktails)
     }
 
     render(){
+
+        let cocktails = this.state.cocktails
+            .filter(drink => {
+                let searchfield = this.state.searchfield.toLowerCase()
+                let drinkName = drink.strDrink.toLowerCase()
+
+                // if there is no searchfield value, render this drink
+                if (!searchfield) {
+                    return true
+                // if this drink's strDrink field includes the search value, render it
+                } else if (drinkName.includes(searchfield)){
+                    return true
+                // otherwise, do not render this drink
+                } else {
+                    return false
+                }
+            })
+            // this will make components for each coctail that passes the search filtering method
+            .map((drink, index) => {
+                return(
+                    <Link to={"/cocktails/" + drink.strDrink} key={index}><Cocktail drink={drink} /></Link>
+                )
+            })
+
         return ( 
             <div className="tc">
                 <SearchBox searchChange={this.onSearchChange}/>
@@ -50,7 +70,7 @@ class Cocktails extends Component {
                 <Route path="/"
                 render = {() => (
                 <div className='drinkCardWrapper'>
-                    {this.state.cocktails}
+                    {cocktails}
                 </div>
                 )}
                 />
